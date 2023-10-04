fb3991512ecee769c0aac82e88abd53840c19aec
jorgemago <jorgero.nunez@gmail.com>
2023-10-04 20:07:31 -0300

    preentrega03

---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent {
+  userForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  get nameControl() {
+    return this.userForm.get('name');
+  }
+
+  get emailControl() {
+    return this.userForm.get('email');
+  }
+}
